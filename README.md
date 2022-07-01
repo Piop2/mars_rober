06826c4b97409d60be2ffece2b447ca22c4a2865
Piop2 <seungyounyou0220@gmail.com>
2022-07-01 21:48:08 +0900

    add rober.py

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mars Rober
+2022 서울고 경희제 META SpaceX의 로버 작품 코드입니다
---
+++ rober.py
@@ -0,0 +1,42 @@
+import pygame
+import gpiozero
+
+# init pygame
+pygame.init()
+pygame.joystick.init()
+
+# video system
+display = pygame.display.set_mode((500, 500))
+clock = pygame.time.Clock()
+
+# 모터 포트 번호 입력좀;;;
+# motor_f1 = gpiozero.Motor(0, 0)
+# motor_f2 = gpiozero.Motor(0, 0)
+# motor_b1 = gpiozero.Motor(0, 0)
+# motor_b2 = gpiozero.Motor(0, 0)
+
+# controller button
+controller = None
+up = False
+down = False
+left = False
+right = False
+
+running = True
+while running:
+    clock.tick(60)
+
+    try:
+        # set controller
+        controller = pygame.joystick.Joystick(0)
+    except RuntimeError:
+        controller = None
+        pass
+
+    # event
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()
+
+pygame.quit()
