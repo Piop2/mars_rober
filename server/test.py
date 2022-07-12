5a3bb0b4ef91c4944ff7cdfeb814fe95c4e8666f
Piop2 <seungyounyou0220@gmail.com>
2022-07-12 16:03:00 +0900

    test motor

--- server/test.py
+++ server/test.py
@@ -1,8 +1,10 @@
 import gpiozero
 
-motor = gpiozero.Robot(left=(17, 27), right=(5, 6))
+motor1 = gpiozero.Robot(left=(17, 27), right=(5, 6))
+motor2 = gpiozero.Robot(left=(23, 24), right=(25, 16))
 
 while True:
-    motor.forward(1)
+    motor1.forward(1)
+    motor2.forward(1)
 
 
